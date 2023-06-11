36a102f399905c9e2f1dd24c4579cc92b6a46a6d
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-12 03:09:11 +0600

    user api created

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>{
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot7ey7w.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,9 +29,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db("summerCamp").collection("users")
     const classCollection = client.db("summerCamp").collection("classes")
     const teacherCollection = client.db("summerCamp").collection("teachers")
     const cartCollection = client.db("summerCamp").collection("carts")
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
   
     app.get('/classes', async(req, res)=>{
       const result = await classCollection.find().toArray();
