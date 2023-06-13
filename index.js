8b66c2ed188ea48e906a33576a82365f53fce102
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-13 21:58:03 +0600

    patch operation done

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
-
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("summer school is running");
-})
+app.get("/", (req, res) => {
+  res.send("summer school is running");
+});
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot7ey7w.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,70 +20,83 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const usersCollection = client.db("summerCamp").collection("users")
-    const classCollection = client.db("summerCamp").collection("classes")
-    const teacherCollection = client.db("summerCamp").collection("teachers")
-    const cartCollection = client.db("summerCamp").collection("carts")
+    const usersCollection = client.db("summerCamp").collection("users");
+    const classCollection = client.db("summerCamp").collection("classes");
+    const teacherCollection = client.db("summerCamp").collection("teachers");
+    const cartCollection = client.db("summerCamp").collection("carts");
 
-    app.get('/users', async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/users', async(req, res)=>{
-      const user = req.body
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
       console.log(user);
-      const query = {email: user.email}
-      const existingUser = await usersCollection.findOne(query)
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
       console.log(existingUser);
-      if(existingUser){
-        return res.send({message:"user already exists"})
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
-  
-    app.get('/classes', async(req, res)=>{
+    });
+
+    app.get("/classes", async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/teachers', async(req, res)=>{
+    });
+    app.get("/teachers", async (req, res) => {
       const result = await teacherCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/carts', async(req, res)=>{
+    });
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
       console.log(email);
-      const query = {email: email}
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/carts', async(req, res)=>{
-      const item = req.body
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.delete('/carts/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await cartCollection.deleteOne(query)
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,7 +104,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.listen(port, (req, res)=>{
-    console.log(`summer school is listening on port :${port}`);
-})
+app.listen(port, (req, res) => {
+  console.log(`summer school is listening on port :${port}`);
+});
