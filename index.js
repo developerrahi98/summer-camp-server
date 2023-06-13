4d63c7d7816fff6297c8e2297166cac7744913b4
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-13 23:30:34 +0600

    jwt verifying

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -6,6 +7,20 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error : true , message : 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+    if(err){
+      return res.status(401).send({error : true , message :  'unauthorized access'});
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
 
 app.get("/", (req, res) => {
   res.send("summer school is running");
@@ -31,6 +46,12 @@ async function run() {
     const classCollection = client.db("summerCamp").collection("classes");
     const teacherCollection = client.db("summerCamp").collection("teachers");
     const cartCollection = client.db("summerCamp").collection("carts");
+
+    app.post('/jwt', (req,res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn :'1h'} )
+      res.send({ token });
+    })
 
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -70,9 +91,13 @@ async function run() {
       const result = await teacherCollection.find().toArray();
       res.send(result);
     });
-    app.get("/carts", async (req, res) => {
+    app.get("/carts",verifyJWT, async (req, res) => {
       const email = req.query.email;
       console.log(email);
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail) {
+        return  res.status(403).send({error : true , message :  'porviden access'});
+      }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
